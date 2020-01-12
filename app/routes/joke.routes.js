5dffa36f28cce7759467a9a264f2916851e1a33e
Michael Murphy <michael.w.murphy@outlook.com>
2020-01-12 21:46:23 +0000

    Refactored the code in jokes.routes.js"

--- app/routes/joke.routes.js
+++ app/routes/joke.routes.js
@@ -1,15 +1,14 @@
 module.exports = (app) => {
     const jokes = require('../controllers/joke.controller.js')
 
-    app.post('/jokes', jokes.create)
-
-    app.get('/jokes', jokes.findAll)
+    app.route('/jokes')
+        .post(jokes.create)
+        .get(jokes.findAll)
 
     app.get('/jokes/random', jokes.findOneRandom)
 
-    app.get('/jokes/:jokeId', jokes.findOne)
-
-    app.put('/jokes/:jokeId', jokes.update)
-    
-    app.delete('/jokes/:jokeId', jokes.delete)
+    app.route('/jokes/:jokeId')
+        .get(jokes.findOne)
+        .put(jokes.update)
+        .delete(jokes.delete)
 }
