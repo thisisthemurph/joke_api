6aa1a605e781942fdc49d1ae178fd65ec51a113d
Michael Murphy <michael.w.murphy@outlook.com>
2020-02-10 16:54:43 +0000

    Moved server code from app.js to server.js

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 
-const jokeRoutes = require('./app/routes/joke.routes.js')
-
 app = express()
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
@@ -10,16 +8,7 @@ app.use(bodyParser.json())
 // Connect to the database
 require('./db.js')
 
-// Define a route
-app.get('/', (req, res) => {
-    res.json({"message": "Welcome to the Jokes API."})
-})
-
 // Set up the routes for /jokes
-jokeRoutes(app)
+require('./app/routes/joke.routes.js')(app)
 
-// Listen for requests on the following port
-const PORT = 3000
-app.listen(PORT, () => {
-    console.log('Server is listening on port ' + PORT)
-})
+module.exports = app
